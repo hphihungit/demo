9162f8c4348df32bf8ce558b29a3d2afefc183d3
hphihungit <phihunghoang.1310@gmail.com>
2024-08-02 13:53:46 +0700

    update

---
+++ backend/src/main/java/com/example/asassa/AsassaApplication.java
@@ -0,0 +1,13 @@
+package com.example.asassa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsassaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsassaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/asassa/AsassaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asassa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsassaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
